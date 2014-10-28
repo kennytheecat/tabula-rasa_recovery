7e09a35293ea3991af9db4a946d013403de26159
Kenny Scott <kennytheecat@gmail.com>
2014-10-27 23:32:14 -0700

    More archive changes

--- content.php
+++ content.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -34,26 +34,16 @@
 				$categories_list = get_the_category_list( __( ', ', 'tabula-rasa' ) );
 				if ( $categories_list && tr_categorized_blog() ) :
 			?>
+			<?php if ( !is_archive()) { ?>
 			<span class="cat-links">
 				<?php printf( __( 'Posted in %1$s', 'tabula-rasa' ), $categories_list ); ?>
 			</span>
+			<?php } ?>
 			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'tabula-rasa' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'tabula-rasa' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
 		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'tabula-rasa' ), __( '1 Comment', 'tabula-rasa' ), __( '% Comments', 'tabula-rasa' ) ); ?></span>
 		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', 'tabula-rasa' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
