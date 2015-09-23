5a943784fbe20c10b36ef612b683cc83d9b4d10f
Kenny Scott <kennytheecat@gmail.com>
2015-09-23 10:52:44 -0700

    added featured recovery centers to the sidebar

--- sidebar.php
+++ sidebar.php
@@ -9,10 +9,32 @@
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'tabula-rasa' ); ?></h1>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
+				<h1 class="widget-title"><?php _e( 'Featured Recovery Centers', 'tabula-rasa' ); ?></h1>
+				
+			<?php //while ( have_posts() ) : the_post(); 
+				$args = array(
+					'posts_per_page' => 3,
+					'post_type' => 'rehabs',
+					'orderby' => 'rand'
+				);
+    $query = new WP_Query( $args  );
+    while( $query->have_posts() ):
+    $query->the_post();
+		$post_id = $post->ID;
+		?>
+					<header class="entry-header">
+					<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="entry-thumbnail">
+							<?php the_post_thumbnail('compare'); ?>
+						</div>
+						<?php endif; ?>
+					</header><!-- .entry-header -->
+					<div class="philosophy">
+						<?php the_excerpt(); ?>
+					</div>
+			<?php endwhile; // end of the loop. ?>
+			
 			</aside>
 			
 		<?php endif; // end sidebar widget area ?>
